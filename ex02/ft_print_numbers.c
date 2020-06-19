49762df7cfaf1f3c7b1424231ec1c1a28b4cebfa
Pravir Rama <prama@c4r9s1.wethinkcode.co.za>
2020-06-19 08:47:58 +0200

    Completed ex02

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,26 +6,23 @@
 /*   By: prama <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 09:53:15 by prama             #+#    #+#             */
-/*   Updated: 2020/06/18 09:55:21 by prama            ###   ########.fr       */
+/*   Updated: 2020/06/19 08:38:20 by prama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+void	ft_putchar(char c);
 
-void    ft_print_numbers(void)
+void	ft_print_numbers(void)
+
 {
-    char    number;
+	char	number;
 
-    number = '0';
-    while (number <= '9')
-    {
-        putchar(number);
-        number++;
-    }
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
 }
 
-int main(void)
-{
-    ft_print_numbers();
-    return 0;
-}
+
