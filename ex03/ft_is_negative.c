fe675bf2865bfe73c19b9558ab327cd94bb143d0
Pravir Rama <prama@c4r9s1.wethinkcode.co.za>
2020-06-23 09:10:10 +0200

    Completed ex03

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,11 +6,18 @@
 /*   By: prama <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 09:59:45 by prama             #+#    #+#             */
-/*   Updated: 2020/06/19 08:44:38 by prama            ###   ########.fr       */
+/*   Updated: 2020/06/23 09:09:26 by prama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+
+/*
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}*/
 
 void	ft_is_negative(int n)
 {
@@ -20,4 +27,9 @@ void	ft_is_negative(int n)
 		ft_putchar('N');
 }
 
-
+/*
+int		main(void)
+{
+	ft_is_negative(0);
+	return (0);
+}	*/
